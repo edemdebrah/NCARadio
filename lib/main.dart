66682f0e7712dc5d4ea72cab59d08f290cea78d3
DavidAgyakwa <56321266+davidAg9@users.noreply.github.com>
2023-09-01 20:14:17 +0000

    fixed uninitialized dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dab_online_radio/core/utilities/dependency_injector.dart';
+import 'package:dab_online_radio/features/homePage/logic/radio_manager_logic/radio_manager_bloc.dart';
 import 'package:dab_online_radio/features/homePage/logic/station_player_logic/station_player_bloc.dart';
 import 'package:dab_online_radio/features/homePage/view/homePage.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await initailizeAllDependencies();
   runApp(const NCARadio());
 }
 
@@ -16,8 +17,15 @@ class NCARadio extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<StationPlayerBloc>(
-      create: (context) => dep<StationPlayerBloc>(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<StationPlayerBloc>(
+          create: (context) => dep<StationPlayerBloc>(),
+        ),
+        BlocProvider<RadioManagerBloc>(
+          create: (context) => dep<RadioManagerBloc>(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'NCA Radio',
